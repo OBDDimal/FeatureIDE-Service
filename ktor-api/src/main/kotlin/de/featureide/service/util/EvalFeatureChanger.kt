abda324b7603b7be92e49d7061cbd01367a77c76
Lukas Ostheimer <131896156+LukasOsti@users.noreply.github.com>
2024-11-02 14:01:01 +0100

    feat: ready implemented for eval

--- ktor-api/src/main/kotlin/de/featureide/service/util/EvalFeatureChanger.kt
+++ ktor-api/src/main/kotlin/de/featureide/service/util/EvalFeatureChanger.kt
@@ -6,7 +6,8 @@ import de.ovgu.featureide.fm.core.FeatureModelAnalyzer
 import de.ovgu.featureide.fm.core.base.IFeature
 import de.ovgu.featureide.fm.core.base.IFeatureModel
 import de.ovgu.featureide.fm.core.base.IFeatureStructure
-import de.ovgu.featureide.fm.core.base.impl.FeatureStructure
+import de.ovgu.featureide.fm.core.base.impl.Constraint
+import de.ovgu.featureide.fm.core.base.impl.FMFactoryManager
 import de.ovgu.featureide.fm.core.init.FMCoreLibrary
 import de.ovgu.featureide.fm.core.init.LibraryManager
 import de.ovgu.featureide.fm.core.io.IPersistentFormat
@@ -16,6 +17,10 @@ import de.ovgu.featureide.fm.core.job.monitor.NullMonitor
 import kotlinx.cli.ArgParser
 import kotlinx.cli.ArgType
 import kotlinx.cli.required
+import org.prop4j.And
+import org.prop4j.Implies
+import org.prop4j.Literal
+import org.prop4j.Not
 import java.io.BufferedReader
 import java.io.File
 import java.io.FileReader
@@ -23,8 +28,8 @@ import java.io.IOException
 import java.nio.file.Files
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.text.DecimalFormat
 import java.util.concurrent.TimeUnit
+
 
 object EvalFeatureChanger {
 
@@ -49,7 +54,11 @@ object EvalFeatureChanger {
 
         val constraint by parser.option(ArgType.Boolean, shortName = "c", description = "Add constraint")
 
-        val features by parser.option(ArgType.String, shortName = "f", description = "Name of the features splitted by a , .")
+        val features by parser.option(
+            ArgType.String,
+            shortName = "f",
+            description = "Name of the features splitted by a , ."
+        )
 
 
         parser.parse(args)
@@ -69,14 +78,15 @@ object EvalFeatureChanger {
 
                 var model = FeatureModelManager.load(Paths.get(file.path))
 
-                val statTriple = getDeadCoreFeatures(model)
+                var statTriple = getDeadCoreFeatures(model)
 
-                println("Before Slicing")
+                println("Before Mandatory")
 
                 println("Features: " + model.features.size)
                 println("DeadFeatures: " + statTriple.first.size)
                 println("FalseOptional: " + statTriple.second.size)
                 println("CoreFeatures: " + statTriple.third.size)
+                println("Constraints: " + model.constraints.size)
 
                 val format: IPersistentFormat<IFeatureModel> = DIMACSFormat()
 
@@ -92,10 +102,10 @@ object EvalFeatureChanger {
 
                 var configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
 
-                println("Configurations: " + configurations )
+                println("Configurations: " + configurations)
                 var digits = configurations.length
-                var firstFour = configurations.substring(0,1) + "." + configurations.substring(1,4)
-                println(firstFour + "E" + (digits-1))
+                var firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                println(firstFour + "E" + (digits - 1))
 
                 val featuresToLook = features?.split(",")
                 var featuresToSlice = ArrayList<String>()
@@ -108,12 +118,15 @@ object EvalFeatureChanger {
 
                 model = slice(model, featuresToSliceArray)
 
-                println("After Slicing")
+                println("After Mandatory")
+
+                statTriple = getDeadCoreFeatures(model)
 
                 println("Features: " + model.features.size)
                 println("DeadFeatures: " + statTriple.first.size)
                 println("FalseOptional: " + statTriple.second.size)
                 println("CoreFeatures: " + statTriple.third.size)
+                println("Constraints: " + model.constraints.size)
 
                 newPath = "$output/${file.nameWithoutExtension}_sliced.${format.suffix}"
 
@@ -130,20 +143,297 @@ object EvalFeatureChanger {
                 println("Configurations: " + configurations)
 
                 digits = configurations.length
-                firstFour = configurations.substring(0,1) + "." + configurations.substring(1,4)
-                println(firstFour + "E" + (digits-1))
+                firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                println(firstFour + "E" + (digits - 1))
 
-            } else if (mandatory == true){
+            } else if (mandatory == true) {
 
-                if (relationShip == "and"){
+                if (relationShip == "and") {
 
-                } else if (relationShip == "or"){
+                    val model = FeatureModelManager.load(Paths.get(file.path))
 
-                } else if (relationShip == "alt"){
+                    var statTriple = getDeadCoreFeatures(model)
+
+                    println("Before Mandatory")
+
+                    println("Features: " + model.features.size)
+                    println("DeadFeatures: " + statTriple.first.size)
+                    println("FalseOptional: " + statTriple.second.size)
+                    println("CoreFeatures: " + statTriple.third.size)
+                    println("Constraints: " + model.constraints.size)
+
+                    val format: IPersistentFormat<IFeatureModel> = DIMACSFormat()
+
+                    var newPath = "$output/${file.nameWithoutExtension}.${format.suffix}"
+
+                    saveFeatureModel(
+                        model,
+                        newPath,
+                        format,
+                    )
+
+                    var file = File(newPath)
+
+                    var configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                    println("Configurations: " + configurations)
+                    var digits = configurations.length
+                    var firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                    println(firstFour + "E" + (digits - 1))
+
+                    val featuresToLook = features?.split(",")
+
+                    if (featuresToLook != null) {
+                        for (feature in featuresToLook) {
+                            val featureStructure = model.getFeature(feature).structure
+                            featureStructure.isMandatory = true
+                        }
+                    }
+
+                    println("After Mandatory")
+
+                    statTriple = getDeadCoreFeatures(model)
+
+                    println("Features: " + model.features.size)
+                    println("DeadFeatures: " + statTriple.first.size)
+                    println("FalseOptional: " + statTriple.second.size)
+                    println("CoreFeatures: " + statTriple.third.size)
+                    println("Constraints: " + model.constraints.size)
+
+                    newPath = "$output/${file.nameWithoutExtension}_sliced.${format.suffix}"
+
+                    saveFeatureModel(
+                        model,
+                        newPath,
+                        format,
+                    )
+
+                    file = File(newPath)
+
+                    configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                    println("Configurations: " + configurations)
+
+                    digits = configurations.length
+                    firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                    println(firstFour + "E" + (digits - 1))
+
+                } else if (relationShip == "or") {
+
+                    val model = FeatureModelManager.load(Paths.get(file.path))
+
+                    var statTriple = getDeadCoreFeatures(model)
+
+                    println("Before Mandatory")
+
+                    println("Features: " + model.features.size)
+                    println("DeadFeatures: " + statTriple.first.size)
+                    println("FalseOptional: " + statTriple.second.size)
+                    println("CoreFeatures: " + statTriple.third.size)
+                    println("Constraints: " + model.constraints.size)
+
+                    val format: IPersistentFormat<IFeatureModel> = DIMACSFormat()
+
+                    var newPath = "$output/${file.nameWithoutExtension}.${format.suffix}"
+
+                    saveFeatureModel(
+                        model,
+                        newPath,
+                        format,
+                    )
+
+                    var file = File(newPath)
+
+                    var configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                    println("Configurations: " + configurations)
+                    var digits = configurations.length
+                    var firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                    println(firstFour + "E" + (digits - 1))
+
+                    val featuresToLook = features?.split(",")
+
+                    if (featuresToLook != null) {
+                        for (feature in featuresToLook) {
+                            val featureStructure = model.getFeature(feature).structure
+                            featureStructure.setAnd()
+                            for (child in featureStructure.children) {
+                                child.isMandatory = true
+                            }
+                        }
+                    }
+
+                    println("After Mandatory")
+
+                    statTriple = getDeadCoreFeatures(model)
+
+                    println("Features: " + model.features.size)
+                    println("DeadFeatures: " + statTriple.first.size)
+                    println("FalseOptional: " + statTriple.second.size)
+                    println("CoreFeatures: " + statTriple.third.size)
+                    println("Constraints: " + model.constraints.size)
+
+                    newPath = "$output/${file.nameWithoutExtension}_sliced.${format.suffix}"
+
+                    saveFeatureModel(
+                        model,
+                        newPath,
+                        format,
+                    )
+
+                    file = File(newPath)
+
+                    configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                    println("Configurations: " + configurations)
+
+                    digits = configurations.length
+                    firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                    println(firstFour + "E" + (digits - 1))
+
+                } else if (relationShip == "alt") {
+
+                    val model = FeatureModelManager.load(Paths.get(file.path))
+
+                    var statTriple = getDeadCoreFeatures(model)
+
+                    println("Before Mandatory")
+
+                    println("Features: " + model.features.size)
+                    println("DeadFeatures: " + statTriple.first.size)
+                    println("FalseOptional: " + statTriple.second.size)
+                    println("CoreFeatures: " + statTriple.third.size)
+                    println("Constraints: " + model.constraints.size)
+
+                    val format: IPersistentFormat<IFeatureModel> = DIMACSFormat()
+
+                    var newPath = "$output/${file.nameWithoutExtension}.${format.suffix}"
+
+                    saveFeatureModel(
+                        model,
+                        newPath,
+                        format,
+                    )
+
+                    var file = File(newPath)
+
+                    var configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                    println("Configurations: " + configurations)
+                    var digits = configurations.length
+                    var firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                    println(firstFour + "E" + (digits - 1))
+
+                    val featuresToLook = features?.split(",")
+
+                    if (featuresToLook != null && featuresToLook.size == 1) {
+                        val child = model.getFeature(featuresToLook[0])
+                        val featureStructure = child.structure
+                        val parent = featureStructure.parent.feature.name
+                        model.addConstraint(Constraint(model, Implies(Literal(parent), Literal(child.name))))
+                    }
+
+                    println("After Mandatory")
+
+                    statTriple = getDeadCoreFeatures(model)
+
+                    println("Features: " + model.features.size)
+                    println("DeadFeatures: " + statTriple.first.size)
+                    println("FalseOptional: " + statTriple.second.size)
+                    println("CoreFeatures: " + statTriple.third.size)
+                    println("Constraints: " + model.constraints.size)
+
+                    newPath = "$output/${file.nameWithoutExtension}_sliced.${format.suffix}"
+
+                    saveFeatureModel(
+                        model,
+                        newPath,
+                        format,
+                    )
+
+                    file = File(newPath)
+
+                    configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                    println("Configurations: " + configurations)
+
+                    digits = configurations.length
+                    firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                    println(firstFour + "E" + (digits - 1))
 
                 }
 
-            } else if (constraint == true){
+            } else if (constraint == true) {
+
+                val model = FeatureModelManager.load(Paths.get(file.path))
+
+                var statTriple = getDeadCoreFeatures(model)
+
+                println("Before Slicing")
+
+                println("Features: " + model.features.size)
+                println("DeadFeatures: " + statTriple.first.size)
+                println("FalseOptional: " + statTriple.second.size)
+                println("CoreFeatures: " + statTriple.third.size)
+                println("Constraints: " + model.constraints.size)
+
+                val format: IPersistentFormat<IFeatureModel> = DIMACSFormat()
+
+                var newPath = "$output/${file.nameWithoutExtension}.${format.suffix}"
+
+                saveFeatureModel(
+                    model,
+                    newPath,
+                    format,
+                )
+
+                var file = File(newPath)
+
+                var configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                println("Configurations: " + configurations)
+                var digits = configurations.length
+                var firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                println(firstFour + "E" + (digits - 1))
+
+                val featuresToLook = features?.split(",")
+
+                if (featuresToLook != null && featuresToLook.size == 2) {
+
+                    val feature1 = model.getFeature(featuresToLook[0])
+                    val feature2 = model.getFeature(featuresToLook[1])
+
+                    model.addConstraint(Constraint(model, Not(And(Literal(feature1.name), Literal(feature2.name)))))
+
+                }
+
+                println("After Constraint")
+
+                statTriple = getDeadCoreFeatures(model)
+
+                println("Features: " + model.features.size)
+                println("DeadFeatures: " + statTriple.first.size)
+                println("FalseOptional: " + statTriple.second.size)
+                println("CoreFeatures: " + statTriple.third.size)
+                println("Constraints: " + model.constraints.size)
+
+                newPath = "$output/${file.nameWithoutExtension}_sliced.${format.suffix}"
+
+                saveFeatureModel(
+                    model,
+                    newPath,
+                    format,
+                )
+
+                file = File(newPath)
+
+                configurations = getNumberOfConfigurationsWithDDnife(outputCSV, file)
+
+                println("Configurations: " + configurations)
+
+                digits = configurations.length
+                firstFour = configurations.substring(0, 1) + "." + configurations.substring(1, 6)
+                println(firstFour + "E" + (digits - 1))
 
             }
 
@@ -152,12 +442,16 @@ object EvalFeatureChanger {
 
     }
 
-    fun getDeadCoreFeatures (model: IFeatureModel) : Triple<List<IFeature>, List<IFeature>, List<IFeature>>{
+    fun getDeadCoreFeatures(model: IFeatureModel): Triple<List<IFeature>, List<IFeature>, List<IFeature>> {
         val featureModelAnalyzer = FeatureModelAnalyzer(model)
-        return Triple(featureModelAnalyzer.getDeadFeatures(NullMonitor()), featureModelAnalyzer.getFalseOptionalFeatures(NullMonitor()),featureModelAnalyzer.getCoreFeatures(NullMonitor()))
+        return Triple(
+            featureModelAnalyzer.getDeadFeatures(NullMonitor()),
+            featureModelAnalyzer.getFalseOptionalFeatures(NullMonitor()),
+            featureModelAnalyzer.getCoreFeatures(NullMonitor())
+        )
     }
 
-    fun getNumberOfConfigurationsWithDDnife(outputCSV: String, dimacsPath: File): String{
+    fun getNumberOfConfigurationsWithDDnife(outputCSV: String, dimacsPath: File): String {
         val rt = Runtime.getRuntime()
 
         var csvPath = "${outputCSV}/${dimacsPath.nameWithoutExtension}"
@@ -173,7 +467,7 @@ object EvalFeatureChanger {
 
         while ((br.readLine().also { line = it }) != null) {
             val str = line!!.split(",".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
-            if (str[2].toDouble().equals(1.0)){
+            if (str[2].toDouble().equals(1.0)) {
                 configurations = str[1]
                 break
             }
@@ -199,9 +493,11 @@ object EvalFeatureChanger {
         val featureList = mutableListOf(featureStructure.feature.name)
         if (featureStructure.children.size > 0) {
             featureStructure.children.forEach { child ->
-                featureList.addAll(getAllChildrenForStructure(
-                    child
-                ))
+                featureList.addAll(
+                    getAllChildrenForStructure(
+                        child
+                    )
+                )
             }
             return featureList
         } else {
